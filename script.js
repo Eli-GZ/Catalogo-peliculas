54022b2ffb13a00f77fd07ab5fd53c635a0276f6
Elias <elias.d.gonzalez25@gmail.com>
2024-04-01 22:43:25 -0300

    details

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('searchButton').addEventListener('click', buscarMovies)
+let apiKey = 'fbd9d085bd91a5b1e29b16b0cb32d864';
+let url =
+  function buscarMovies() {
+    let searchInput = document.getElementById('searchInput').value
+    fetch()
+
+  }
