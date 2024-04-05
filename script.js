0f4dc37649915d934784ad5eee3cb03673c4ed6f
Elias <elias.d.gonzalez25@gmail.com>
2024-04-04 21:18:15 -0300

    details

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-document.getElementById('searchButton').addEventListener('click', buscarMovies)
+document.getElementById('searchButton').addEventListener('click', searchMovies)
 let api_key = 'fbd9d085bd91a5b1e29b16b0cb32d864';
 let urlBase = 'https://api.themoviedb.org/3/search/movie'
 
-  function buscarMovies() {
+  function searchMovies() {
     let searchInput = document.getElementById('searchInput').value
     fetch(`${urlBase}?query=${searchInput}&api_key=${api_key}`)
     .then(response=>response.json())
     .then(response=>console.log(response))
   }
+  
